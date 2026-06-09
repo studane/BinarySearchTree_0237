aa3a1a0db023427b497cd45d21cfe3fcc2adac21
studane <zhidaneprabowoguzlend@gmail.com>
2026-06-09 11:10:53 +0700

    menambah fungsi insert

--- searchtree.cpp
+++ searchtree.cpp
@@ -19,3 +19,26 @@ public:
 };
 
 class binarytree
+{
+public:
+    node *root;
+
+    binarytree()
+    {
+        root = NULL;
+    }
+
+    void insert(string element)
+    {
+        node *newnode = new node(element, NULL, NULL);
+        
+        newnode->info = element;
+        newnode->leftchild = NULL;
+        newnode->rightchild = NULL;
+
+        node *parent = NULL;
+        node *currentnode = NULL;
+
+        search(element, parent, currentnode);
+
+        

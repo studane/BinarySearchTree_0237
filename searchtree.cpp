f6cd1149808012f906f6d789311094e48debe06b
studane <zhidaneprabowoguzlend@gmail.com>
2026-06-09 11:09:53 +0700

    memberi akses public dan memberi variabelnya

--- searchtree.cpp
+++ searchtree.cpp
@@ -3,3 +3,18 @@
 using namespace std;
 
 class node
+{
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
